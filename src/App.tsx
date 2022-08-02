0446839c07e4e23f8e3275894595b1da6387ef47
mixed_nuts_nuts <french0719@gmail.com>
2022-08-02 23:17:52 +0700

    form

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,23 @@ const App = () => {
       <form action="">
         <h1>Login Form</h1>
         <hr />
-        <div></div>
+        <div className="uiForm">
+          <div className="formField">
+            <label>User Name</label>
+            {/* name属性..JSで使う! */}
+            <input type="text" placeholder="UserName" name="username" />
+          </div>
+          <div className="formField">
+            <label>Mail Address</label>
+            {/* name属性..JSで使う! */}
+            <input type="text" placeholder="MailAddress" name="mailAddress" />
+          </div>
+          <div className="formField">
+            <label>PassWord</label>
+            {/* name属性..JSで使う! */}
+            <input type="text" placeholder="PassWord" name="password" />
+          </div>
+        </div>
       </form>
     </div>
   );
