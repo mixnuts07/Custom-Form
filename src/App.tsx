2ccc00de8b1aef75596516f66cca34b118778784
mixed_nuts_nuts <french0719@gmail.com>
2022-08-03 11:05:25 +0700

    state

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 const App = () => {
+  type inittialType = {
+    username: string;
+    mailAddress: string;
+    password: string;
+  };
+  const initialValues: inittialType = {
+    username: "",
+    mailAddress: "",
+    password: "",
+  };
+  const [formValues, setFormValues] = useState(initialValues);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    // name キーを上書きしている！！
+    setFormValues({ ...formValues, [name]: value });
+    console.log(formValues);
+  };
   return (
     <div className="formContainer">
       <form action="">
         <h1>Login Form</h1>
         <hr />
+
         <div className="uiForm">
           <div className="formField">
             <label>User Name</label>
             {/* name属性..JSで使う! */}
-            <input type="text" placeholder="UserName" name="username" />
+            <input
+              type="text"
+              placeholder="UserName"
+              name="username"
+              onChange={(event) => handleChange(event)}
+            />
           </div>
           <div className="formField">
             <label>Mail Address</label>
